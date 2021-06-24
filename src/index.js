b72380e33b90f9171a52e1f5d85d92b8d0a57ceb
Jorgen Arnes <jorgen.arnes@gmail.com>
2021-06-24 16:21:37 +0200

    attached onclick events to each <a> tags

--- src/index.js
+++ src/index.js
@@ -1,45 +1,57 @@
 import { agrees } from './fakeData';
 
-
 var htmlElement = document.getElementById('agrees-container');
 
 let newHTML = "";
 
-for (let index = 0; index < agrees.length; index++) {
-    const elements = `<div>
-                        <input type="checkbox" name="DeepInsightConsent" value="1"></input>
-                        <input type="hidden" name="DeepInsightConsent" value="0"></input>
-                        <label>${agrees[index].agree}
-                            <a href="#" style="cursor: pointer" class="modal-btn" id="modal-btn">Vis detaljer</a>
-                        </label>
-                        <div id="consent_dialog_0" class="modal centered" style="width:70%;background-color:#F4FFEF;border:1px dotted black;">
-                            <p></p>
-                            <h2>${agrees[index].modalHeading}</h2>
-                            <p>This Privacy Policy describes Our polici. <a href="http://privacy.entraos.io/" target="_blank">Vis detaljer</a></p>
-                            <p></p>
-                            <button style="cursor: pointer" class="hide-consent" id="consent_hide_0">Close</button>
-                        </div>
-                    </div>`;
-    newHTML += elements;
-}
-
-htmlElement.innerHTML = newHTML;
-
-
-let modalOpen = document.getElementById('modal-btn');
-let modal = document.querySelector('.modal');
-
-modalOpen.addEventListener('click', function(e) {
-    modal.style.visibility = "visible";
-    modal.style.opacity = "1";
-}, false)
-
-let modalClose = document.querySelector('.hide-consent');
+    for (let index = 0; index < agrees.length; index++) {
+       console.log(agrees[index].modalHeading);
+        const elements = `<div>
+                            <input type="checkbox" name="DeepInsightConsent" value="1"></input>
+                            <input type="hidden" name="DeepInsightConsent" value="0"></input>
+                            <label>${agrees[index].agree}
+                                <a href="#" style="cursor: pointer" class="modal-btn" id="modal-btn">Vis detaljer</a>
+                            </label>
+                            <div id="consent_dialog_0" class="modal centered" style="width:70%;background-color:#F4FFEF;border:1px dotted black;">
+                                <p></p>
+                                <h2>${agrees[index].modalHeading}</h2>
+                                <p>This Privacy Policy describes Our polici. <a href="http://privacy.entraos.io/" target="_blank">Vis detaljer</a></p>
+                                <p></p>
+                                <button style="cursor: pointer" class="hide-consent" id="consent_hide_0">Close</button>
+                            </div>
+                        </div>`;
+                        
+        newHTML += elements;
         
-modalClose.addEventListener('click', function() {
-    modal.style.visibility = "invisible";
-    modal.style.opacity = "0";
-}, false)    
+    }
+    
+    htmlElement.innerHTML = newHTML;
+
+    let modalOpen = document.querySelectorAll('.modal-btn');
+    
+    for(let i = 0; i < modalOpen.length; i++) {
+       
+        modalOpen[i].onclick = function(e) {
+            let modal = document.querySelector('.modal');
+            console.log(i)
+            modal.style.visibility = "visible";
+            modal.style.opacity = "1";
+            e.preventDefault();
+
+        let modalClose = document.querySelector('.hide-consent');
+        
+        modalClose.addEventListener('click', (e) => {
+            modal.style.visibility = "invisible";
+            modal.style.opacity = "0";
+            e.preventDefault();
+        }, false)    
+        }
+    }
+   
+
+   
+
+
        
        
   
