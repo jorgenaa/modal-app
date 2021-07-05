59bf1c716fed03117520b96f0c9f85114bd18082
Jorgen Arnes <jorgen.arnes@gmail.com>
2021-07-05 21:30:08 +0200

    wrapped the template literal inside a function

--- src/index.js
+++ src/index.js
@@ -4,28 +4,31 @@ var htmlElement = document.getElementById('agrees-container');
 
 let newHTML = "";
 
+function createModal(agrees) {
     for (let index = 0; index < agrees.length; index++) {
-       console.log(agrees[index].modalHeading);
-        const elements = `<div>
-                            <input type="checkbox" name="DeepInsightConsent" value="1"></input>
-                            <input type="hidden" name="DeepInsightConsent" value="0"></input>
-                            <label>${agrees[index].agree}
-                                <a href="#" style="cursor: pointer" class="modal-btn" id="modal-btn">Vis detaljer</a>
-                            </label>
-                            <div id="consent_dialog_${index}" class="modal centered" style="width:70%;background-color:#F4FFEF;border:1px dotted black;">
-                                <p></p>
-                                <h2 class="title">${agrees[index].modalHeading}</h2>
-                                <p>This Privacy Policy describes Our polici. <a href="http://privacy.entraos.io/" target="_blank">Vis detaljer</a></p>
-                                <p></p>
-                                <button style="cursor: pointer" class="hide-consent" id="consent_hide_${index}">Close</button>
-                            </div>
-                        </div>`;             
-        newHTML += elements;
-    }
-    
-    
-    htmlElement.innerHTML = newHTML;
-
+        console.log(agrees[index].modalHeading);
+         const elements = `<div>
+                             <input type="checkbox" name="DeepInsightConsent" value="1"></input>
+                             <input type="hidden" name="DeepInsightConsent" value="0"></input>
+                             <label>${agrees[index].agree}
+                                 <a href="#" style="cursor: pointer" class="modal-btn" id="modal-btn">Vis detaljer</a>
+                             </label>
+                             <div id="consent_dialog_${index}" class="modal centered" style="width:70%;background-color:#F4FFEF;border:1px dotted black;">
+                                 <p></p>
+                                 <h2 class="title">${agrees[index].modalHeading}</h2>
+                                 <p>This Privacy Policy describes Our polici. <a href="http://privacy.entraos.io/" target="_blank">Vis detaljer</a></p>
+                                 <p></p>
+                                 <button style="cursor: pointer" class="hide-consent" id="consent_hide_${index}">Close</button>
+                             </div>
+                         </div>`;             
+         newHTML += elements;
+     }
+     
+     
+     htmlElement.innerHTML = newHTML;
+}
+   
+createModal(agrees);
 
     let modalOpen = document.querySelectorAll('.modal-btn');
     
